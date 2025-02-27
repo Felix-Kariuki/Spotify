55fc5d27d30a9877c5963659a569337754faddb5
Felix-Kariuki <felixmurimi280@gmail.com>
2025-02-27 19:23:32 +0300

    Initial Commit

---
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by Felix kariuki on 27/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
